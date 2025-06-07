a4be88dba3d9e9e4a1a8265a582e15974ea6196d
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-07 20:34:40 +0600

    stat

---
+++ backend/src/controller/stat.controller.js
@@ -0,0 +1,48 @@
+import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getStats } from "../controller/stat.controller.js";
+
+const router = Router();
+
+router.get("/", protectRoute, requireAdmin, getStats);
+
+export default router;
+import { Album } from "../models/album.model.js";
+import { Song } from "../models/song.model.js";
+import { User } from "../models/user.model.js";
+
+export const getStats = async (req, res, next) => {
+	try {
+		const [totalSongs, totalAlbums, totalUsers, uniqueArtists] = await Promise.all([
+			Song.countDocuments(),
+			Album.countDocuments(),
+			User.countDocuments(),
+
+			Song.aggregate([
+				{
+					$unionWith: {
+						coll: "albums",
+						pipeline: [],
+					},
+				},
+				{
+					$group: {
+						_id: "$artist",
+					},
+				},
+				{
+					$count: "count",
+				},
+			]),
+		]);
+
+		res.status(200).json({
+			totalAlbums,
+			totalSongs,
+			totalUsers,
+			totalArtists: uniqueArtists[0]?.count || 0,
+		});
+	} catch (error) {
+		next(error);
+	}
+};
