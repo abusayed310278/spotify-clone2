bcb11d818af33f7c94939e0e033012e179c105c7
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-07 22:37:02 +0600

    add token setup

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -17,18 +17,30 @@ const AuthProvider = () => {
     const [loading,setLoading]=useState(true)
 
     useEffect(()=>{
+
         const initAuth=async()=>{
             try{
                 const token=await getToken()
                 updateApiToken(token)
 
             }catch(error:any){
+                updateApiToken(null)
                 console.log("Error in auth provider",error)
+            }finally{
+                setLoading(false)
             }
         }
+
         initAuth()
+        
     },[getToken])
 
+
+    if(loading)return(
+        <div className='h-screen w-full flex items-center justify-center'>
+            <Loader />
+        </div>
+    )
 
 
   return (
