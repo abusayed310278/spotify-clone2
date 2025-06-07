9f6ddf8fa149599792d16276862b40a39c1569c9
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-07 21:10:25 +0600

    error free

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -1,15 +1,3 @@
-import { Router } from "express";
-import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controller/song.controller.js";
-import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
-
-const router = Router();
-
-router.get("/", protectRoute, requireAdmin, getAllSongs);
-router.get("/featured", getFeaturedSongs);
-router.get("/made-for-you", getMadeForYouSongs);
-router.get("/trending", getTrendingSongs);
-
-export default router;
 import { Song } from "../models/song.model.js";
 
 export const getAllSongs = async (req, res, next) => {
--- backend/src/controller/stat.controller.js
+++ backend/src/controller/stat.controller.js
@@ -1,12 +1,3 @@
-import { Router } from "express";
-import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
-import { getStats } from "../controller/stat.controller.js";
-
-const router = Router();
-
-router.get("/", protectRoute, requireAdmin, getStats);
-
-export default router;
 import { Album } from "../models/album.model.js";
 import { Song } from "../models/song.model.js";
 import { User } from "../models/user.model.js";
