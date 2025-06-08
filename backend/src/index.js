5b109adb7763e42d04f85073c3103ef7deffef80
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-08 22:24:14 +0600

    login with google

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,63 +1,66 @@
-import express from 'express'
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import { clerkMiddleware } from '@clerk/express'
-import fileUpload from "express-fileupload"
-import path from "path"
+import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
 
-import userRoutes from "./routes/user.route.js"
-import authRoutes from "./routes/auth.route.js"
-import adminRoutes from "./routes/admin.route.js"
-import songRoutes from "./routes/song.route.js"
-import statRoutes from "./routes/stat.route.js"
-import albumRoutes from "./routes/album.route.js"
-import connectToMongoDB from './lib/db.js'
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import songRoutes from "./routes/song.route.js";
+import statRoutes from "./routes/stat.route.js";
+import albumRoutes from "./routes/album.route.js";
+import connectToMongoDB from "./lib/db.js";
 
-
-dotenv.config()
-const app=express()
-const __dirname=path.resolve()
+dotenv.config();
+const app = express();
+const __dirname = path.resolve();
 
 app.use(
-	cors({
-		origin: "http://localhost:3000",
-		credentials: true,
-	})
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
 );
 
-app.use(express.json()) //to parse req.body
+app.use(express.json()); //to parse req.body
 
-
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 app.use(
-	fileUpload({
-		useTempFiles: true,
-		tempFileDir: path.join(__dirname, "tmp"),
-		createParentPath: true,
-		limits: {
-			fileSize: 10 * 1024 * 1024, // 10MB  max file size
-		},
-	})
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB  max file size
+    },
+  })
 );
 
+const PORT = process.env.PORT;
 
-const PORT=process.env.PORT
-
-
-app.use("/api/users",userRoutes)
-app.use("/api/auth",authRoutes)
-app.use("/api/admin",adminRoutes)
-app.use("/api/songs",songRoutes)
-app.use("/api/albums",albumRoutes)
-app.use("/api/stats",statRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
 
 //error handler
-app.use((err,req,res,next)=>{
-	res.status(500).json({message: process.env.NODE_ENV ==="production" ? "Internal Server Error" : err.message })
-})
+app.use((err, req, res, next) => {
+  res
+    .status(500)
+    .json({
+      message:
+        process.env.NODE_ENV === "production"
+          ? "Internal Server Error"
+          : err.message,
+    });
+});
 
-app.listen(PORT,()=>{
-    connectToMongoDB()
-    console.log("server is running on port:"+PORT);
-})
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log("server is running on port:" + PORT);
+});
