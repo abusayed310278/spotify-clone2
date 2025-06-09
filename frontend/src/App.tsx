98d6bda6dfe440680161cd3ecdb97f5444f4e18e
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-09 07:34:47 +0600

    create homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,13 @@
-
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import { Toaster } from "react-hot-toast";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
     <>
-     
-
       {/* <header>
         <SignedOut>
           <SignInButton>
@@ -24,17 +22,24 @@ function App() {
         </SignedIn>
       </header> */}
       <Routes>
-          <Route path="/" element={<HomePage />}/>
-          <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
             signUpForceRedirectUrl={"/auth-callback"}
-          />}/>
-          <Route path="/auth-callback" element={<AuthCallbackPage />}/>
+            />
+          }
+        />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<HomePage />} />
 
 
+        </Route>
       </Routes>
 
       <Toaster />
-
     </>
   );
 }
