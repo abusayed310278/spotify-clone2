cf8a5063881ca6f77d13a9063c4fee0139043c6b
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-09 07:43:06 +0600

    add chat page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "react-hot-toast";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
 
         <Route element={<MainLayout />}>
           <Route path='/' element={<HomePage />} />
+          <Route path='/chat' element={<ChatPage />} />
 
 
         </Route>
