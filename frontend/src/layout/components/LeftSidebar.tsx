fac5a8c70f516a07d714990cf946682227a0bf97
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-09 20:30:56 +0600

    adding loading when playlist is loaded

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -1,3 +1,4 @@
+import PlaylistSkeleton from "@/components/skeletons/PlaylistSkeleton";
 import { buttonVariants } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
@@ -6,8 +7,11 @@ import { HomeIcon, Library, MessageCircle } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const LeftSidebar = () => {
+  const isLoading = true;
+
   return (
     <div className="h-full flex flex-col gap-2">
+
       {/* Navigation menu  */}
       <div className="rounded-lg bg-zinc-900 p-4">
         <div className="space-y-2">
@@ -44,17 +48,25 @@ const LeftSidebar = () => {
 
       {/* Library section  */}
       <div className="flex-1 rounded-lg bg-zinc-900 p-4">
-        
         <div className="flex items-center justify-between mb-4">
-            <div className="flex items-center text-white px-2">
-              <Library className="size-5 mr-2"/>
-              <span className="hidden md:inline">Playlists</span>
-            </div>
+          <div className="flex items-center text-white px-2">
+            <Library className="size-5 mr-2" />
+            <span className="hidden md:inline">Playlists</span>
+          </div>
         </div>
 
-        <ScrollArea></ScrollArea>      
-        
+        <ScrollArea className="h-[calc(100vh-300px)]">
+          <div className="space-y-2">
+            {isLoading ? 
+            (<PlaylistSkeleton />) : 
+            (
+              "some "
+            )}
+          </div>
+        </ScrollArea>
 
+
+        
       </div>
     </div>
   );
