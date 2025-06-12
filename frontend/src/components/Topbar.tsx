4279b178f51eb64fbb958ffb09d48724f549ee19
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-12 07:51:09 +0600

    add admin dashboard button outlined

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,12 +1,19 @@
-import { SignedIn, SignedOut, SignOutButton, UserButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import { LayoutDashboard } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButtons from "./SignInOAuthButtons";
 import { useAuthStore } from "@/stores/useAuthStore";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 const Topbar = () => {
   const { isAdmin } = useAuthStore();
-  console.log({isAdmin});
+  console.log({ isAdmin });
   return (
     <div
       className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75
@@ -14,28 +21,31 @@ const Topbar = () => {
     "
     >
       <div className="flex gap-2 items-center">
-        <img src="/spotify.png"  className="size-8" alt='spotify-logo'/>
+        <img src="/spotify.png" className="size-8" alt="spotify-logo" />
         Spotify
-        </div>
+      </div>
 
       <div className="flex items-center gap-4">
         {isAdmin && (
           <Link to={"/admin"}>
-            <LayoutDashboard className="size-4 mr-2" />
-            Admin Dashboard
+            {isAdmin && (
+              <Link to={"/admin"} className={cn(buttonVariants({variant:"outline"}))}>
+                <LayoutDashboard className="size-4 mr-2" />
+                Admin Dashboard
+              </Link>
+            )}
           </Link>
         )}
 
-        <SignedIn>
+        {/* <SignedIn>
           <SignOutButton />
-        </SignedIn>
+        </SignedIn> */}
 
         <SignedOut>
           <SignInOAuthButtons />
         </SignedOut>
 
         <UserButton />
-
       </div>
     </div>
   );
