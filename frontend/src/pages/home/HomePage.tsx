39c2c9b3a50ff212cc70a055cc4048aba7547400
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-12 08:06:54 +0600

    get data from useMusic store to homepage

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,6 +1,25 @@
 import Topbar from "@/components/Topbar"
+import { useMusicStore } from "@/stores/useMusicStore";
+import { useEffect } from "react";
 
 const HomePage = () => {
+
+  const {
+		fetchFeaturedSongs,
+		fetchMadeForYouSongs,
+		fetchTrendingSongs,
+		isLoading,
+		madeForYouSongs,
+		featuredSongs,
+		trendingSongs,
+	} = useMusicStore();
+
+  	useEffect(() => {
+		fetchFeaturedSongs();
+		fetchMadeForYouSongs();
+		fetchTrendingSongs();
+	}, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
   return (
     <div className="rounded-md overflow-hidden">
       
