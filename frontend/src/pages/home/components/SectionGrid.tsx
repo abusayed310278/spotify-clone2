efc1a67ac5bed2ff9e112de5cc2ac4aaaa02adb7
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-13 23:21:10 +0600

    add playbutton

--- frontend/src/pages/home/components/SectionGrid.tsx
+++ frontend/src/pages/home/components/SectionGrid.tsx
@@ -1,6 +1,7 @@
 import type { Song } from "@/types";
 import SectionGridSkeleton from "./SectionGridSkeleton";
 import { Button } from "@/components/ui/button";
+import PlayButton from "./PlayButton";
 
 type SectionGridProps = {
   title: string;
@@ -40,7 +41,7 @@ const SectionGrid = ({ songs, title, isLoading }: SectionGridProps) => {
 									group-hover:scale-105"
                 />
               </div>
-              {/* <PlayButton song={song} /> */}
+              <PlayButton song={song} />
             </div>
             <h3 className="font-medium mb-2 truncate">{song.title}</h3>
             <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
